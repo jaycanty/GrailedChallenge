3b85d31feb2b8401877c36843ff7e6a75705588c
jay <jay@jays-MBP.hsd1.nj.comcast.net>
2017-05-09 18:36:38 -0400

    Add data model

---
+++ GrailedChallenge/DataModel.swift
@@ -0,0 +1,16 @@
+//
+//  DataModel.swift
+//  GrailedChallenge
+//
+//  Created by jay on 5/9/17.
+//  Copyright © 2017 jay. All rights reserved.
+//
+
+import Foundation
+
+
+class DataModel {
+    
+    
+    
+}
