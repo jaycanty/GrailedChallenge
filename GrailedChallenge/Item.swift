5cc39153b28fd1124422f07c85de32436fa4915a
jay <jay@jays-MBP.hsd1.nj.comcast.net>
2017-05-09 22:17:26 -0400

    Add item

---
+++ GrailedChallenge/Item.swift
@@ -0,0 +1,21 @@
+//
+//  Item.swift
+//  GrailedChallenge
+//
+//  Created by jay on 5/9/17.
+//  Copyright © 2017 jay. All rights reserved.
+//
+
+import Foundation
+
+
+struct Item {
+
+    var createdAt: Date
+    var title: String
+    var desc: String
+    var designerName: String
+    var size: String
+    var price: Int
+    var photo: String
+}
