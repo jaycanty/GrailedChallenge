e40836c7eb69cdb084044ef41f88ce363aa4ee15
jay <jay@jays-MBP.hsd1.nj.comcast.net>
2017-05-10 11:35:07 -0400

    Add get data

--- GrailedChallenge/DataModel.swift
+++ GrailedChallenge/DataModel.swift
@@ -11,15 +11,24 @@ import Foundation
 
 class DataModel {
     
-    var currentPage: Int = 1
-
+    var currentPage: Int = 1 {
+        didSet {
+            if currentPage == 1 {
+                items.removeAll()
+            }
+        }
+    }
+    var items = [Item]()
     func getData(complete: Result<[Item]>) {
-        
         let service = DataService()
-        
         service.fetchListData(for: currentPage) { result in
-         
-            
+            switch result {
+            case .error(_):
+                complete(result)
+            case let .success(data):
+                items += data
+                complete(.success(items))
+            }
         }
     }
 }
