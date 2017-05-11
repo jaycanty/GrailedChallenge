505a2edb030a2434f3a3c80185696979dbaa0d3d
jay <jay@jays-MBP.hsd1.nj.comcast.net>
2017-05-11 10:57:48 -0400

    Add image manager

---
+++ GrailedChallenge/ImageManager.swift
@@ -0,0 +1,41 @@
+//
+//  ImageManager.swift
+//  GrailedChallenge
+//
+//  Created by jay on 5/11/17.
+//  Copyright © 2017 jay. All rights reserved.
+//
+
+import Foundation
+
+
+class ImageManager {
+
+    static let shared = ImageManager()
+    
+    private lazy var queue: OperationQueue = {
+        let q = OperationQueue()
+        q.name = "Image Queue"
+        q.maxConcurrentOperationCount = 1
+        return q
+    }()
+    let cache = NSCache<NSString, NSData>()
+    
+    
+    func image(for urlString: String, complete: @escaping (Data?)->()) {
+        queue.addOperation {
+            if let data = self.cache.object(forKey: urlString as NSString) {
+                complete(data as Data)
+            } else if let url = URL(string: urlString) {
+                do {
+                    let data = try Data(contentsOf: url)
+                    self.cache.setObject(data as NSData, forKey: urlString as NSString)
+                    complete(data)
+                } catch {
+                    print(error)
+                    complete(nil)
+                }
+            }
+        }
+    }
+}
